3a6a8a4affb447c11d3c24366c793d87c88392fb
saroro <hbhj4633@naver.com>
2022-11-14 13:13:21 +0900

    Add PetInfo Database

---
+++ app/src/main/java/com/example/RoomDataBase/PetInfo.java
@@ -0,0 +1,39 @@
+package com.example.RoomDataBase;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class PetInfo {
+
+    @PrimaryKey(autoGenerate = true)
+    public int idx;
+
+    @ColumnInfo(name = "name")
+    public String name;
+
+    @ColumnInfo(name = "exp")
+    public int exp;
+
+    @ColumnInfo(name = "max_exp")
+    public int max_exp;
+
+    @ColumnInfo(name = "is_owned")
+    public boolean is_owned;
+
+    @ColumnInfo(name = "flavor_text")
+    public String flavor_text;
+
+    @ColumnInfo(name = "line1")
+    public String line1;
+
+    @ColumnInfo(name = "line2")
+    public String line2;
+
+    @ColumnInfo(name = "next_idx")
+    public int next_idx;
+
+    @ColumnInfo(name = "img_source")
+    public int img_source;
+}
