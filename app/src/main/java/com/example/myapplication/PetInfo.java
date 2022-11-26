f0ffdda13a5de5069985e615f32ea74902463213
saroro <hbhj4633@naver.com>
2022-11-26 12:56:26 +0900

    펫 정보부분 수정

--- app/src/main/java/com/example/myapplication/PetInfo.java
+++ app/src/main/java/com/example/myapplication/PetInfo.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -13,68 +14,39 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.csv.PetInfoCSV;
+import com.example.csv.factor.PetInfoCsvFactor;
+
+import java.util.List;
+
 public class PetInfo extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pinfo, container, false);
 
-        if(getArguments()!=null) {
-            ImageView petImage = view.findViewById(R.id.petImage);
-            TextView petName = view.findViewById(R.id.petName);
-            TextView petInfo = view.findViewById(R.id.petInfo);
-            int petImageId;
-            int petNameId;
-            int petInfoId;
-
-            int index = getArguments().getInt("INDEX", 1);
-            switch (index) {
-                case 1:
-                    petImageId = R.drawable.pet_1_1;
-                    petNameId= R.string.petName_1_1;
-                    petInfoId = R.string.petInfo_1_1;
-                    break;
-
-                case 2:
-                    petImageId = R.drawable.pet_1_2;
-                    petNameId= R.string.petName_1_2;
-                    petInfoId = R.string.petInfo_1_2;
-                    break;
-
-                case 3:
-                    petImageId = R.drawable.pet_1_3;
-                    petNameId= R.string.petName_1_3;
-                    petInfoId = R.string.petInfo_1_3;
-                    break;
-
-                case 4:
-                    petImageId = R.drawable.pet_2_1;
-                    petNameId= R.string.petName_2_1;
-                    petInfoId = R.string.petInfo_2_1;
-                    break;
-
-                case 5:
-                    petImageId = R.drawable.pet_2_2;
-                    petNameId= R.string.petName_2_2;
-                    petInfoId = R.string.petInfo_2_2;
-                    break;
-
-                case 6:
-                    petImageId = R.drawable.pet_2_3;
-                    petNameId= R.string.petName_2_3;
-                    petInfoId = R.string.petInfo_2_3;
-                    break;
-
-                default:
-                    petImageId = 0;
-                    petNameId= 0;
-                    petInfoId = 0;
+        Context context = getContext();
+        List<PetInfoCsvFactor> petInfoCsv;
+        try{
+            assert context != null;
+            petInfoCsv = (new PetInfoCSV()).getPetInfo(context);
+            if(getArguments()!=null) {
+                ImageView petImage = view.findViewById(R.id.petImage);
+                TextView petName = view.findViewById(R.id.petName);
+                TextView petInfo = view.findViewById(R.id.petInfo);
+                int index = getArguments().getInt("INDEX", 1)-1;
+                PetInfoCsvFactor petInfoContainer = petInfoCsv.get(index);
+                int petImageId = this.getResources().getIdentifier(petInfoContainer.img_dir, "drawable", context.getPackageName());
+                petImage.setImageResource(petImageId);
+                petName.setText(petInfoContainer.name);
+                petInfo.setText(petInfoContainer.flavor_text);
             }
-
-            petImage.setImageResource(petImageId);
-            petName.setText(petNameId);
-            petInfo.setText(petInfoId);
         }
+        catch(Exception e){
+
+        }
+
+
 
         Button backBtn = view.findViewById(R.id.backButton);
         backBtn.setOnClickListener(new View.OnClickListener() {
